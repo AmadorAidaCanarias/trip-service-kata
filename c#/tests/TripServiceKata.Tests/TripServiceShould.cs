3f1de14e3958ca4b8c101b71871bdf38119392fc
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-06-08 08:21:39 +0100

    Test: Devolver Excepción UserNotLoggedInException cuando loggedUser es null Rojo

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,21 @@
-﻿namespace TripServiceKata.Tests
+﻿using System;
+using TripServiceKata.Exception;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact(DisplayName ="Should Return UserNotLoggedInException When Not Logged User")]
+        public void return_null_when_send_null() {
+            // Arrange
+            var tripService = new TripService();
+
+            // Act
+            void Result() => tripService.GetTripsByUser(null);
+
+            // Assert
+            Assert.Throws<UserNotLoggedInException>((Action) Result);
+        }
     }
 }
