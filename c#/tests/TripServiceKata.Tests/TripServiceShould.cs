a2f81de98c6de29be310621d91bfd6118fb43a8c
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-06-14 07:30:06 +0100

    Rojo: devolver una lista de Trip cuando es amigo

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -6,25 +6,23 @@ using TripServiceKata.Exception;
 using Xunit;
 
 namespace TripServiceKata.Tests {
-    public class TripServiceShould
-    {
-        [Fact(DisplayName ="Should Return UserNotLoggedInException When Not Logged User")]
+    public class TripServiceShould {
+        [Fact(DisplayName = "Should Return UserNotLoggedInException When Not Logged User")]
         public void return_null_when_send_null() {
             // Arrange
             var userLoginInformation = Substitute.For<IUserLoginInformation>();
-            userLoginInformation.GetLoggedUser().Returns((IUser) null);
+            userLoginInformation.GetLoggedUser().Returns((IUser)null);
             var tripService = new TripService(userLoginInformation);
-            
+
             // Act
             void Result() => tripService.GetTripsByUser(null);
 
             // Assert
-            Assert.Throws<UserNotLoggedInException>((Action) Result);
+            Assert.Throws<UserNotLoggedInException>((Action)Result);
         }
 
         [Fact(DisplayName = "Should Return Empty Trip List When not Is Friend")]
-        public void return_empty_list_trip_when_not_is_friend()
-        {
+        public void return_empty_list_trip_when_not_is_friend() {
             var userLoginInformation = Substitute.For<IUserLoginInformation>();
             var loggedUser = new User();
             userLoginInformation.GetLoggedUser().Returns(loggedUser);
@@ -35,6 +33,21 @@ namespace TripServiceKata.Tests {
             var userTrips = tripService.GetTripsByUser(user);
 
             Assert.Equal(userTrips, new List<Trip>());
+        }
+
+        [Fact(DisplayName = "Should return trip list with data when is friend")]
+        public void return_trip_list_with_data_when_is_friend() {
+            var userLoginInformation = Substitute.For<IUserLoginInformation>();
+            var loggedUser = new User();
+            userLoginInformation.GetLoggedUser().Returns(loggedUser);
+            var user = Substitute.For<IUser>();
+            user.IsFriend(loggedUser).Returns(true);
+
+            var tripService = new TripService(userLoginInformation);
+
+            var userTrips = tripService.GetTripsByUser(user);
+
+            Assert.Equal(userTrips, new List<Trip>() { new Trip() });
         }
     }
 }
